1863c2412465012dc8e46ba6add84325cc5b476a
LukeJenningsMQ <61956662+LukeJenningsMQ@users.noreply.github.com>
2023-09-08 01:21:44 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,68 @@ import ProductPage from './ProductPage';
 import ShoppingCartPage from './ShoppingCartPage';
 import OrderPage from './OrderPage';
 
-function App() {  
+function App() {
+  const [session, setSession] = useState(null)
+  const [userInfo, setUserInfo] = useState([])
+
+    const baseurl = 'https://individualproject-azure-app.azurewebsites.net/api/session'
+     
+  
+    const fetchSession = () => {
+      axios.get(baseurl)
+      .then((response) => {
+        console.log("response: ", response.data)
+        setSession(response.data)
+      })
+    }
+
+  
+    useEffect(() => {
+      fetchSession()
+    },[])
+
+    useEffect(() => {
+      if (session !== null) {
+        // This will only run if session is not null
+        console.log("Updated session:", session);
+        const userurl = 'https://individualproject-azure-app.azurewebsites.net/api/users/' + session.user_id
+        axios.get(userurl)
+          .then((response) => {
+          console.log("response: ", response.data)
+          setUserInfo(response.data)
+        })
+      }
+  }, [session])
 
   return (
-    <p>Hello there</p>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+      <div className='Banner'>
+      <img className="Logo" src='WebLogo.png' alt='website logo'></img>
+      </div>
+      <nav>
+      <ul className="Navigation">
+        <Link style={{ textDecoration: 'none' }} to="/"><li>Home</li></Link>
+        <Link style={{ textDecoration: 'none' }} to="/Cart"><li>Cart</li></Link>
+        <Link style={{ textDecoration: 'none' }} to="/Orders"><li>Orders</li></Link>
+        <li className='log'>{userInfo.first_name} {userInfo.last_name} logged In</li>
+      </ul>
+      </nav>
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<ShoppingCartPage uInfo={userInfo}></ShoppingCartPage>} />
+        <Route path="/Orders" element={<OrderPage uInfo={userInfo} />} />
+        <Route path="/:id" element={<ProductPage uInfo={userInfo}/>} />
+      </Routes>
+      </header>
+      <footer>
+        <p>&copy; 2023 Luke Jennings. All rights reserved.</p>
+      </footer>
+      </div>
+    </Router>
     
   );
 }
