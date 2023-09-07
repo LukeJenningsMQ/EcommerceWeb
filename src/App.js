c87ab3c0892d17a3bc0a88765f5ec59e07116d78
LukeJenningsMQ <61956662+LukeJenningsMQ@users.noreply.github.com>
2023-09-08 00:33:55 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
       if (session !== null) {
         // This will only run if session is not null
         console.log("Updated session:", session);
-        const userurl = '/api/users/' + session.user_id
+        const userurl = 'https://individualproject-azure-app.azurewebsites.net/api/users/' + session.user_id
         axios.get(userurl)
           .then((response) => {
           console.log("response: ", response.data)
